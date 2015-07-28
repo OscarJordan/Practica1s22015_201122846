9869ce835126377314fd6a37e27e7d206dbe3c7e
OscarPC <OscarPC@Oscar>
2015-07-27 20:25:20 -0600

    [EDD]Practica1-2-2015

---
+++ src/edd/practica1/EDDPractica1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edd.practica1;
+
+/**
+ *
+ * @author OscarPC
+ */
+public class EDDPractica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Agregar nuevo = new Agregar();
+        nuevo.setLocationRelativeTo(null);
+        nuevo.setVisible(true);
+        
+    }
+    
+}
