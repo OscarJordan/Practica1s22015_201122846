4629a7ea338bb33c77f85c0224c4bcbaf9b90b23
OscarPC <OscarPC@Oscar>
2015-08-10 21:36:57 -0600

    [EDD]Practica1-2-2015

--- src/edd/practica1/Tablero.java
+++ src/edd/practica1/Tablero.java
@@ -7,6 +7,12 @@
 package edd.practica1;
 
 import java.awt.Color;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
@@ -23,12 +29,126 @@ public class Tablero extends javax.swing.JFrame {
     ListaMatriz lista = new ListaMatriz();
     int columna = 0;
     int fila = 0;
+    String cadenamatriz1;
+    String cadenamatriz2;
        
     
     public Tablero() {
         initComponents();
         InicializarMatriz();
     }
+    
+     public void LlenarTxtLista ()
+    {
+    cadenamatriz1 = ""; 
+    cadenamatriz2 = "";
+    try
+    {
+    FileWriter salida = null;
+    File archivo = new File("c:\\Salida\\SalidaMatriz.txt");
+    salida =  new FileWriter(archivo);
+        
+    salida.write("digraph G");
+    salida.write("\r\n");
+    salida.write("{");
+    salida.write("\r\n");
+    
+    NodoMatriz recorrer;
+    recorrer = lista.cabeza;
+    //JOptionPane.showMessageDialog(null, "Ya dentro del método, la cabeza es: " + recorrer.nombre);
+    while(recorrer != null)
+    {
+        //JOptionPane.showMessageDialog(null, "While 1");
+    if(recorrer.sig == null)
+    {
+    cadenamatriz1 = cadenamatriz1 + "-> x" + recorrer.posx + "_" + "y" + recorrer.posy + ";";
+    recorrer = recorrer.sig;
+    }
+    else
+    {
+    cadenamatriz1 = cadenamatriz1 + "x" + recorrer.posx + "_" + "y" + recorrer.posy;
+    recorrer = recorrer.sig;
+    }
+    }
+    
+    NodoMatriz recorrer2;
+    recorrer2 = lista.cabeza;
+    
+    while(recorrer2 != null)
+    {
+    //JOptionPane.showMessageDialog(null, "While 2");
+    if (recorrer2 == lista.cabeza)
+    {
+    cadenamatriz1 = cadenamatriz1 + "x" + recorrer2.posx + "_" + "y" + recorrer2.posy;
+    recorrer2 = recorrer2.abajo;    
+    }
+        
+        else if(recorrer2.abajo != null)
+    {
+    cadenamatriz1 = cadenamatriz1 + "-> x" + recorrer2.posx + "_" + "y" + recorrer2.posy;
+    recorrer2 = recorrer2.abajo;    
+     
+    
+    }
+    
+    else
+    {
+    cadenamatriz1 = cadenamatriz1 + "-> x" + recorrer2.posx + "_" + "y" + recorrer2.posy + ";";
+    recorrer2 = recorrer2.abajo;
+    }
+    }
+    
+    salida.write(cadenamatriz1);
+    salida.write("\r\n");
+    salida.write("\r\n");
+    salida.write(cadenamatriz2);
+    salida.write("\r\n");
+    salida.write("}");
+    salida.close();
+    }
+        catch(IOException e)
+        {
+        
+        
+        }
+}
+    
+    
+    public void GraficarNodosLista()
+    {
+    try {
+      
+      String dotPath = "C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe";
+      
+      String fileInputPath = "C:\\Salida\\SalidaMatriz.txt";
+      String fileOutputPath = "C:\\Salida\\ImagenMatriz.jpg";
+      
+      String tParam = "-Tjpg";
+      String tOParam = "-o";
+        
+      String[] cmd = new String[5];
+      cmd[0] = dotPath;
+      cmd[1] = tParam;
+      cmd[2] = fileInputPath;
+      cmd[3] = tOParam;
+      cmd[4] = fileOutputPath;
+                  
+      Runtime rt = Runtime.getRuntime();
+      
+      rt.exec(cmd);
+      
+      
+    } catch (Exception ex) 
+    {
+      ex.printStackTrace();
+    }
+    finally 
+    {
+        
+    }
+    }
+    
+    
 
     public void InicializarMatriz()
     {
@@ -106,6 +226,7 @@ public class Tablero extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         PanTablero = new javax.swing.JPanel();
         PanLista = new javax.swing.JPanel();
+        Btn_Graf = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -139,6 +260,13 @@ public class Tablero extends javax.swing.JFrame {
             .addGap(0, 305, Short.MAX_VALUE)
         );
 
+        Btn_Graf.setText("Graficar Matriz");
+        Btn_Graf.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn_GrafActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -152,7 +280,10 @@ public class Tablero extends javax.swing.JFrame {
                         .addComponent(PanLista, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(153, 153, 153)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(212, 212, 212)
+                        .addComponent(Btn_Graf)))
                 .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -164,11 +295,37 @@ public class Tablero extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(PanLista, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(PanTablero, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(44, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
+                .addComponent(Btn_Graf)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void Btn_GrafActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_GrafActionPerformed
+        // TODO add your handling code here:
+        LlenarTxtLista();
+        GraficarNodosLista();
+        
+        try    
+            {
+            Thread.sleep(1000);
+            } 
+        catch(InterruptedException ex) 
+            {
+            Thread.currentThread().interrupt();
+            }
+        
+        File f = new File("C:\\Salida\\ImagenMatriz.jpg");
+        Desktop dt = Desktop.getDesktop();
+        try {
+            dt.open(f);
+        } catch (IOException ex) {
+            Logger.getLogger(Agregar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }//GEN-LAST:event_Btn_GrafActionPerformed
 
     /**
      * @param args the command line arguments
@@ -206,6 +363,7 @@ public class Tablero extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Btn_Graf;
     private javax.swing.JPanel PanLista;
     private javax.swing.JPanel PanTablero;
     private javax.swing.JLabel jLabel1;
